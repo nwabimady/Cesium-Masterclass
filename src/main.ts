853253b316008ffa6ea1314d707b42233232ddd3
nwabimady <nwabimady@gmail.com>
2023-12-14 23:51:47 +0000

    IFC loader and CesiumMatrix sync

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3OTdmM
 
 const viewer = new Viewer("cesiumContainer", {
   terrain: Terrain.fromWorldTerrain(),
-  //useDefaultRenderLoop: false,
+  useDefaultRenderLoop: false,
 })
 
 const buildingTileset = await createOsmBuildingsAsync();
@@ -71,4 +71,104 @@ const center = Cartesian3.fromDegrees(
     }
   };
   const Polygon = viewer.entities.add(entity);
+
+  type Object3D = {
+    threeMesh: THREE.Object3D,
+    minWGS84: number[],
+    maxWGS84: number[],
+  }
+  const _3Dobjects: Object3D[] = [];
+
+  const ifcLoader = new OBC.FragmentIfcLoader(components);
+  const file = await fetch("asd.ifc");
+  const data = await file.arrayBuffer();
+  const buffer = new Uint8Array(data);
+  const model = await ifcLoader.load(buffer, "asd");
   
+  for(const child of model.children) {
+    child.rotation.x = Math.PI / 2;
+    child.position.z += 65;
+  }
+
+  scene.add(model);
+  const ifcObject: Object3D = {
+    threeMesh: model,
+    minWGS84: minWGS84,
+    maxWGS84: maxWGS84,
+  }
+_3Dobjects.push(ifcObject)
+
+renderer.onBeforeUpdate.add(() => {
+  viewer.render();
+
+  const width = window.innerWidth;
+  const height =  window.innerHeight;
+  camera.aspect = width / height;
+
+  const perspectiveFrustum = viewer.camera.frustum as THREE.PerspectiveFrustum;
+  if(perspectiveFrustum.fovy === undefined) return;
+
+  camera.fov = CesiumMath.toDegrees(perspectiveFrustum.fovy);
+  camera.updateProjectionMatrix();
+
+  const cartToVec = function(cart: any) {
+    return new THREE.Vector3(cart.x, cart.y, cart.z);
+  }
+  // Configure Three.js meshes to stand against globe center position up direction
+  for( const id in _3Dobjects){
+    const minWGS84 = _3Dobjects[id].minWGS84;
+    const maxWGS84 = _3Dobjects[id].maxWGS84;
+
+  // convert lat/long center position to Cartesian3
+  const center = Cartesian3.fromDegrees(
+    (minWGS84[0] + maxWGS84[0]) / 2, 
+    (minWGS84[1] + maxWGS84[1]) / 2
+    );
+
+  // get forward direction for orienting model
+  const centerHigh = Cartesian3.fromDegrees(
+    (minWGS84[0] + maxWGS84[0]) / 2, 
+    (minWGS84[1] + maxWGS84[1]) / 2,
+    1
+    );
+    const centerHighVec = new THREE.Vector3(
+      centerHigh.x,
+      centerHigh.y,
+      centerHigh.z
+    );
+  // use direction from bottom left to top left as up-vector
+  const bottomLeft  = cartToVec(
+    Cartesian3.fromDegrees(minWGS84[0], minWGS84[1])
+    );
+  const topLeft = cartToVec(Cartesian3.fromDegrees(minWGS84[0], maxWGS84[1]));
+  const latDir  = new THREE.Vector3()
+    .subVectors(bottomLeft,topLeft )
+    .normalize();
+
+  // configure entity position and orientation
+  _3Dobjects[id].threeMesh.position.set(center.x, center.y, center.z);
+  _3Dobjects[id].threeMesh.lookAt(centerHighVec);
+  _3Dobjects[id].threeMesh.up.copy(latDir);
+}
+  
+// Clone Cesium Camera projection position so the
+  // Three.js Object will appear to be at the same place as above the Cesium Globe
+  camera.matrixAutoUpdate = false;
+   const cvm = viewer.camera.viewMatrix;
+   const civm = viewer.camera.inverseViewMatrix;
+  camera.matrixWorld.set(
+      civm[0], civm[4], civm[8 ], civm[12],
+      civm[1], civm[5], civm[9 ], civm[13],
+      civm[2], civm[6], civm[10], civm[14],
+      civm[3], civm[7], civm[11], civm[15]
+  );
+  camera.matrixWorldInverse.set(
+      cvm[0], cvm[4], cvm[8 ], cvm[12],
+      cvm[1], cvm[5], cvm[9 ], cvm[13],
+      cvm[2], cvm[6], cvm[10], cvm[14],
+      cvm[3], cvm[7], cvm[11], cvm[15]
+  );
+  camera.updateProjectionMatrix();
+
+
+})
